ed138b355e1247954793a9186df2436eb2b9328d
Татьяна <veselovskayt@mail.ru>
2022-01-18 14:18:01 +0700

    Добавьте файлы проекта.

---
+++ WpfTask8/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfTask8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Документ открыт");
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Документ сохранен");
+        }
+        private void ExitCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (textBox != null && textBox.Text.Length == 0)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+      
+        private void ComboBox(object sender, SelectionChangedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                string fontName = (sender as ComboBox).SelectedItem as String;
+                FontFamily font = new FontFamily(fontName);
+                textBox.FontFamily = font;
+            }
+        }
+
+        private void ComboBox1(object sender, SelectionChangedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                string fontSize = (sender as ComboBox).SelectedItem as String;
+                double size = Convert.ToDouble(fontSize);
+                textBox.FontSize = size;
+            }
+        }
+
+            private void ButtonB(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void ButtonI(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+        private void ButtonU(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == null)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+            {
+                textBox.TextDecorations = null;
+            }
+        }
+
+        private void RadioButtonBlack(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+        private void RadioButtonRed(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+
+    }
+}
